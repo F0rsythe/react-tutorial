03c793721d3b3bcc438a9d80dfe6c5a57c28200b
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2023-12-29 07:13:13 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -9,49 +9,23 @@ const App = () => {
       <section className='booklist'>
         {/* <h1>This is a booklist</h1> */}
         <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
       </section>
     </React.Fragment>
   )
 }
 
 const Book = () => {
+  const title = "It ends with us"
+  const author = "Collen Hoover"
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={ewuImage} alt="book-image" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   )
 }
 
-const Image = () => {
-  return (
-    <img
-      src={ewuImage}
-      alt='it ends with us book cover'
-    />
-  )
-}
-
-const Title = () =>{
-  return(
-    <h1>It ends with us</h1>
-  )
-}
-
-const Author = () =>{
-  return(
-    <h4 style={{color: "#617d98", fontSize:"0.75rem", marginTop:"0.25rem" }}>Collen Hoover</h4>
-  )
-}
 ReactDOM.render(<App />, document.getElementById('root'))
 //Alternative
 // const root = ReactDOM.createRoot(document.getElementById('root'))c:\Users\Demilade\Videos\darwin's game\Darwin's Game - 11 [720p x265].mkv c:\Users\Demilade\Videos\darwin's game\Darwin's Game - 09 [720p x265].mkv c:\Users\Demilade\Videos\darwin's game\Darwin's Game - 10 [720p x265].mkv

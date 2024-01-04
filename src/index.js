9874cd6cf0544a4032fa8de7b1a0cfc40b1802d8
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2024-01-04 13:35:16 +0100

    adding classes for props

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import ewuImage from "./images/ewu.png"
+import rohbookCover from "./images/reminders-of-him 1.jpg"
 import "./index.css"
 
+const firstbook = {
+  img: ewuImage,
+  title: "It ends with us",
+  author: "Colleen Hoover"
+}
+const secondbook = {
+  img: rohbookCover,
+  title: 'Reminders of Him',
+  author: 'Colleen Hoover',
+}
 const App = () => {
   return (
     <React.Fragment>
       <section className='booklist'>
         {/* <h1>This is a booklist</h1> */}
-        <Book />
+        <Book img={firstbook.img} title={firstbook.title} author={firstbook.author} />
+        <Book img={secondbook.img} title={secondbook.title} author={secondbook.author} />
       </section>
     </React.Fragment>
   )
 }
 
-const Book = () => {
-  const title = "It ends with us"
-  const author = "Collen Hoover"
+const Book = (props) => {
+
   return (
     <article className='book'>
-      <img src={ewuImage} alt="book-image" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src = {props.img} alt="book-image" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }

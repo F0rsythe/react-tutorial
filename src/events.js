0732301958e457220ac0324e58cf59482b150573
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2024-01-10 14:39:24 +0100

    adding all events as a file

---
+++ src/events.js
@@ -0,0 +1,8 @@
+export const click = (e) => {
+  console.log(e.target)
+  console.log(e)
+  alert('Hello World')
+}
+export const complexBtn = (author) => {
+  alert(`This is the author: ${author}`)
+}

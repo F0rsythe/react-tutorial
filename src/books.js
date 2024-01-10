de160bc801072fb1307a2a0536b18a632da90866
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2024-01-10 14:39:01 +0100

    adding books array as a file

---
+++ src/books.js
@@ -0,0 +1,18 @@
+import ewuImage from "./images/ewu.png"
+import rohbookCover from "./images/reminders-of-him 1.jpg"
+
+// Named Export
+export const books = [
+  {
+    id: 1,
+    img: ewuImage,
+    title: 'It ends with us',
+    author: 'Colleen Hoover',
+  },
+  {
+    id: 2,
+    img: rohbookCover,
+    title: 'Reminders of Him',
+    author: 'Colleen Hoover',
+  },
+]

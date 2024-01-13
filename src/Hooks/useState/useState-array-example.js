83f8223180f3f93c27ce602164bae8e4faae07d2
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2024-01-13 23:43:28 +0100

    update to ArrayExample component

--- src/Hooks/useState/useState-array-example.js
+++ src/Hooks/useState/useState-array-example.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import { data } from '../../data'
 
 const ArrayExample = () => {
-    const [people,setPeople] = useState([]);
-return(
-    <div>
-
-    </div>
-)
+  const [people, setPeople] = useState(data)
+  return (
+    <React.Fragment>
+      {people.map((person) => {
+        const { id, name } = person
+        return <div key={id} className='item'>
+            <h4>
+                {name}
+            </h4>
+        </div>
+      })}
+    </React.Fragment>
+  )
 }
 
-export default ArrayExample;
+export default ArrayExample
